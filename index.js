bf8a956ac9b0aa85c5a1f640cc66a629927951b7
huuduc1501 <lehuuduc15012000@gmail.com>
2021-06-02 22:26:25 +0700

    remove socket.io

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const morgan = require('morgan')
 
 const server = require('http').createServer(app)
 const cors = require('cors')
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*'
-    }
-})
 
 
 const errorHandler = require('./middlewares/errorHandler')
